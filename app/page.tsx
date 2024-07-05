5a7658b522a3fba28dc810c3c91cc37f00e0b643
Florian Ouilhon <ouilhonflorian@outlook.fr>
2024-07-05 19:33:02 +0200

    initialize Facebook SDK for logging

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
 import Image from "next/image";
+import { useEffect } from "react";
+import FBInit from "@/app/components/fbinit";
+import { initFacebookSdk } from "@/app/lib/initFBSDK";
+
+initFacebookSdk().then(Home);
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col">
+      <div className="flex w-full h-16 border-b items-center justify-center border-slate-300">
+        <p className="flex items-center text-2xl md:text-3xl">Medianalytics</p>
+      </div>
+      <FBInit />
       
     </main>
   );
