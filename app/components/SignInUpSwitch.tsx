02bfe2e15f2ad765b3fee84ad53d6dd5be8103fb
Florian Ouilhon <ouilhonflorian@outlook.fr>
2024-07-08 13:14:42 +0200

    Modify sign up form behaviour (password + confirm + email required)

--- app/components/SignInUpSwitch.tsx
+++ app/components/SignInUpSwitch.tsx
@@ -1,13 +1,13 @@
 'use client';
 
-import { SetStateAction, useState } from "react";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 
 export default function SignInUpSwitch() {
     const [signIn, setSignIn] = useState(true);
     const [signUp, setSignUp] = useState(false);
     const [passwordConfirmed, setPasswordConfirmed] = useState(false);
-    const [password, setPassword] = useState('');
-    const [passconf, setPassConf] = useState('');
+    var [password, setPassword] = useState('');
+    var [passconf, setPassConf] = useState('');
     function handleSignIn() {
         setSignIn(true);
         setSignUp(false);
@@ -16,18 +16,29 @@ export default function SignInUpSwitch() {
         setSignIn(false);
         setSignUp(true);
     };
-    function getPassword(p: { target: { value: SetStateAction<string>; }; }) {
-        console.log("password: ", p.target.value);
-        setPassword(p.target.value);
-        setPasswordConfirmed(password == passconf);
-        console.log(passwordConfirmed);
-    }
-    function getPassConf(p: { target: { value: SetStateAction<string>; }; }) {
-        console.log("passconf: ", p.target.value);
-        setPassConf(p.target.value);
-        setPasswordConfirmed(password == passconf);
-        console.log(passwordConfirmed);
-    }
+    const getPassword = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const currentValue = event.target.value;
+        password = currentValue;
+        setPassword(currentValue);
+        setConfirmed();
+    };
+    const getPassConf = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const currentValue = event.target.value;
+        passconf = currentValue;
+        setPassConf(currentValue);
+        setConfirmed
+    };
+    function setConfirmed() {
+        if (password === '' || passconf === '') {
+            setPasswordConfirmed(false);
+        } else {
+            if (password === passconf) {
+                setPasswordConfirmed(true);
+            } else {
+                setPasswordConfirmed(false);
+            }
+        }
+    };
     return (
         <>
             <div className="flex w-full h-16">
@@ -49,14 +60,17 @@ export default function SignInUpSwitch() {
                 }
                 {signUp &&
                 <>
-                    <form action="" method="post" className="">
-                        <input type="text" name="email" id="email" className="w-full h-12 bg-slate-100 rounded px-5 py-5 border border-slate-300 mb-6 outline-none focus:bg-slate-200 transition duration-300 focus:border-blue-500 focus:border-2 focus:scale-110" placeholder="Email address" /><br />
-                        <input type="password" name="password" id="password" className="w-full h-12 bg-slate-100 rounded px-5 py-5 border border-slate-300 outline-none focus:bg-slate-200 transition duration-300 focus:border-blue-500 focus:border-2 focus:scale-110" placeholder="Password" onChange={getPassword} /><br />
+                    <form action="" method="post" className="" onChange={setConfirmed}>
+                        <input type="text" name="email" id="email" className="w-full h-12 bg-slate-100 rounded px-5 py-5 border border-slate-300 mb-6 outline-none focus:bg-slate-200 transition duration-300 focus:border-blue-500 focus:border-2 focus:scale-110" placeholder="Email address" required /><br />
+                        <input type="password" name="password" id="password" className="w-full h-12 bg-slate-100 rounded px-5 py-5 border border-slate-300 outline-none focus:bg-slate-200 transition duration-300 focus:border-blue-500 focus:border-2 focus:scale-110" placeholder="Password" required onChange={(event) => getPassword(event)} /><br />
                         {passwordConfirmed ?
-                            <input type="password" name="confirm-password" id="confirm-password" className="w-full h-12 bg-slate-100 rounded px-5 py-5 border border-green-500 outline-none focus:bg-slate-200 transition duration-300 focus:border-blue-500 focus:border-2 focus:scale-110 mt-6" placeholder="Confirm Password" onChange={getPassConf} /> :
-                            <input type="password" name="confirm-password" id="confirm-password" className="w-full h-12 bg-slate-100 rounded px-5 py-5 border border-red-500 outline-none focus:bg-slate-200 transition duration-300 focus:border-blue-500 focus:border-2 focus:scale-110 mt-6" placeholder="Confirm Password" onChange={getPassConf} />
+                            <input type="password" name="confirm-password" id="confirm-password" className="w-full h-12 bg-slate-100 rounded px-5 py-5 border border-green-500 outline-none focus:bg-slate-200 transition duration-300 focus:border-blue-500 focus:border-2 focus:scale-110 mt-6" placeholder="Confirm Password" required onChange={getPassConf} /> :
+                            <input type="password" name="confirm-password" id="confirm-password" className="w-full h-12 bg-slate-100 rounded px-5 py-5 border border-red-500 outline-none focus:bg-slate-200 transition duration-300 focus:border-blue-500 focus:border-2 focus:scale-110 mt-6" placeholder="Confirm Password" required onChange={getPassConf} />
                         }
-                        <input type="submit" value="Sign Up" className="w-full h-14 rounded bg-slate-200 outline-none mt-12 hover:bg-blue-500 hover:text-white hover:cursor-pointer transition duration-300 focus:bg-blue-600" />
+                        {passwordConfirmed ?
+                            <input type="submit" value="Sign Up" className="w-full h-14 rounded bg-slate-200 outline-none mt-12 hover:bg-blue-500 hover:text-white hover:cursor-pointer transition duration-300 focus:bg-blue-600" /> :
+                            <input type="submit" value="Sign Up" className="w-full h-14 rounded bg-slate-200 outline-none mt-12 hover:cursor-not-allowed text-gray-400 transition duration-300 focus:bg-blue-600" disabled />
+                        }
                     </form>
                 </>
                 }
