3a8c8a5499bf66b10133cdd65d67fe96c4008cb9
Florian Ouilhon <ouilhonflorian@outlook.fr>
2024-07-09 19:38:57 +0200

    Set up PG DB + start work on functions

---
+++ app/api/db.ts
@@ -0,0 +1,33 @@
+import { db } from "@vercel/postgres";
+import { UUID } from "crypto";
+
+const client = await db.connect();
+const bcrypt = require('bcrypt');
+
+async function seedUsers(data = []) {
+  try {
+    await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+
+    const createTable = await client.sql`
+      CREATE TABLE IF NOT EXISTS users (
+        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+        email TEXT NOT NULL UNIQUE,
+        password TEXT NOT NULL
+      );
+    `;
+    console.log('Created "users" table');
+
+    const insertedUsers = await Promise.all(
+      data.map(async (user) => {
+        const hashedPassword = await bcrypt.hash(user.password, 10);
+        return client.sql`
+        INSERT INTO users(id, email, password)
+        VALUES (${user.id}, ${user.email}, ${hashedPassword})
+        ON CONFLICT (id) DO NOTHING;
+        `;
+      }),
+    );
+  } catch (error) {
+    
+  }
+}
