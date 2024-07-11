2ac1921843953d8e13b247fc75e4c752797ca1a5
Florian Ouilhon <ouilhonflorian@outlook.fr>
2024-07-11 23:05:26 +0200

    Little style update

---
+++ app/components/PlusIcon.tsx
@@ -0,0 +1,8 @@
+export default function PlusIcon() {
+  return (
+    <div id="plus" className="absolute items-center align-center w-6 h-6 group-hover:invisible group-hover:delay-0 delay-300">
+      <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-4 h-1 bg-white rounded"></div>
+      <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-1 h-4 bg-white rounded"></div>
+    </div>
+  );
+}
