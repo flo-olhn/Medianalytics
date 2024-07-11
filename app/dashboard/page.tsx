91963ab742465db976cfb32de4607bb9267786e8
Florian Ouilhon <ouilhonflorian@outlook.fr>
2024-07-12 01:45:29 +0200

    Saving userId when authenticated for future requests

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,33 +1,31 @@
 'use client';
 
 import { useSession, signIn } from 'next-auth/react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import NavTop from '../components/dashboard/NavTop';
 import NavRight from '../components/dashboard/NavRight';
-import FacebookLoginButton from '../components/FacebookLoginBtn';
 
 export default function Dashboard() {
     const { data: session, status } = useSession();
+    const [userId, setUserId] = useState<string | null>(null);
 
     useEffect(() => {
         if (status === "unauthenticated") {
           console.log('unauthenticated');
           signIn();
         }
-    }, [status]);
+        if (status === "authenticated" && session?.user) {
+            setUserId(session.user.id);
+        }
+    }, [session, status, userId]);
 
     if (status === "loading") {
         return <p>Loading...</p>;
     }
-
-    if (status === "authenticated") {
-        return (
-            <div className='w-full h-full bg-slate-200'>
-                <NavTop></NavTop>
-                <NavRight></NavRight>
-            </div>
-        )
-    }
-
-    return null;
+    return (
+        <div className='w-full h-full bg-slate-200'>
+            <NavTop></NavTop>
+            <NavRight></NavRight>
+        </div>
+    )
 }
