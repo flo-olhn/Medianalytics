6944a41ecac99b05a147b4dfa0c1124688b107d4
Florian Ouilhon <ouilhonflorian@outlook.fr>
2024-07-13 12:20:22 +0200

    Capture long-lived-access-token

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -12,6 +12,7 @@ export default function Dashboard() {
     const { data: session, status } = useSession();
     const [userId, setUserId] = useState<string | null>(null);
     const [accessToken, setAccessToken] = useState<string | null>(null);
+    const [longLivedToken, setLLT] = useState<string | null>(null);
 
     useEffect(() => {
         if (status === "unauthenticated") {
@@ -24,16 +25,19 @@ export default function Dashboard() {
                 const hash = window.location.hash;
                 if (hash) {
                     const params = new URLSearchParams(hash.substring(1));
-                    const token = params.get('access_token');
-                    if (token) {
-                        setAccessToken(token);
+                    const access_token = params.get('access_token');
+                    const llt = params.get('long_lived_token');
+                    if (access_token) {
+                        setAccessToken(access_token);
+                        setLLT(llt);
                     }
                 }
             };
             getToken();
             console.log(accessToken);
+            console.log(longLivedToken);
         }
-    }, [accessToken, session, status, userId]);
+    }, [accessToken, longLivedToken, session, status, userId]);
 
     if (status === "loading") {
         return <div>Loading...</div>
