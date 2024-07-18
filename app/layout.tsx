117f01a3b512440965b33f40054fdf8c96b0ab21
Florian Ouilhon <ouilhonflorian@outlook.fr>
2024-07-19 01:55:03 +0200

    Remove useless import

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
-import type { Session } from "next-auth";
 
 const inter = Inter({ subsets: ["latin"] });
 
